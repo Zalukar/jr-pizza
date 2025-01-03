4b7bef8d2dbef128920aa2ddf9bc0fb204fb3be0
Victor <victor.ardila0205@gmail.com>
2025-01-03 15:42:33 -0500

    navigate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 
 import './App.css';
